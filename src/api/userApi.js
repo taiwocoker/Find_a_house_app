c68e183d7782eb7ec9e197b99e110bbe0e4c32af
taiwocoker <taiwofcoker@gmail.com>
2020-11-11 13:01:50 +0100

    Add users api related logic

---
+++ src/api/userApi.js
@@ -0,0 +1,13 @@
+import { handleResponse, handleError } from './apiUtils'
+const baseUrl = `${process.env.REACT_APP_SERVER_API_URL}/users`
+export default (body, token) =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(body),
+  })
+    .then(handleResponse)
+    .catch(handleError)
