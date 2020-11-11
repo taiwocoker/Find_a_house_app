6bfd431457bd8876ef9688f2bba1cc137ae43670
taiwocoker <taiwofcoker@gmail.com>
2020-11-11 12:51:02 +0100

    Wrap the app with redux provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import './index.css';
 import App from './components/App';
+import {thunk} from 'redux-thunk';
+
+initialState = {
+  houses: [],
+  loading: true,
+  house: {},
+  user: {},
+  favourite: [],
+};
+
+
+const store = createStore(rootReducer,initialState,applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
