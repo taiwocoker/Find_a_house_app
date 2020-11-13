84404d8556cb66f05aae453181582cddc69e9e10
taiwocoker <taiwofcoker@gmail.com>
2020-11-13 13:40:41 +0100

    Build houses component

--- src/containers/HouseList.js
+++ src/containers/HouseList.js
@@ -1,5 +1,49 @@
-import React from 'react'
-function HouseList() {
-  return <div>Houses</div>
-}
-export default HouseList
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loadHouses } from '../actions/housesAction';
+const HouseList = ({ auth, Houses, loadHouses }) => {
+  const loadApplicationHouses = () => {
+    const token = auth.getAccessToken();
+    loadHouses(token);
+  };
+  React.useEffect(() => {
+    if (Houses && Houses.length < 1) {
+      loadApplicationHouses();
+    }
+  }, []);
+  if (!Houses) {
+    return (
+      <h2 className="section-title">Try Again</h2>
+    );
+  }
+  return (
+    <>
+      <section className="section">
+        {Houses.map(house => (
+          <div className="meals-center" key={house.id}>
+            <img src={`${process.env.REACT_APP_SERVER_BLOB_PATH}${house.img_url}`} alt="house" />
+            <p>{house.name}</p>
+            <p>{house.description}</p>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
+HouseList.defaultProps = {
+  auth: null,
+  Houses: null,
+};
+HouseList.propTypes = {
+  auth: PropTypes.object || null,
+  Houses: PropTypes.arrayOf(PropTypes.object) || null,
+  loadHouses: PropTypes.func.isRequired,
+};
+const mapStateToProps = ({ Houses }) => ({ Houses });
+const mapDispatchToProps = dispatch => ({
+  loadHouses: token => dispatch(loadHouses(token)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(HouseList);
+
+
