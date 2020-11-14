055a0ea18739ed7afbf0b41fb139de3b7ebb833e
taiwocoker <taiwofcoker@gmail.com>
2020-11-14 10:53:08 +0100

    Add favourite reducer to the root reducer

--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux'
 import User from './userReducer'
 import Houses from './housesReducer'
 import House from './houseReducer'
+import Loading from './loadingReducer'
+import Favourites from './favouriteReducer'
 export default combineReducers({
-  User, Houses, House, 
+  User, Houses, House, Loading, Favourites, 
 })
