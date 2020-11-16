bc45b83985733a3f847e4b0c0664cc7029bc43e7
taiwocoker <taiwofcoker@gmail.com>
2020-11-16 12:18:51 +0100

    Add lading to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
+const Navbar = ({ auth, Loading }) => {
+  const { isAuthenticated, login, logout } = auth
 
-const Navbar = ({ auth }) => {
-  const { isAuthenticated, login, logout } = auth;
+  if (Loading) return null;
 
   return (
-    <header className="sticky-top">
-      <nav className="navbar navbar-expand-lg navbar-light bg-secondary p-2 px-md-5">
-        <Link to="/" className="navbar-brand color">
+    <header className='sticky-top'>
+      <nav className='navbar navbar-expand-lg navbar-light bg-secondary p-2 px-md-5'>
+        <Link to='/' className='navbar-brand color'>
           FindIt
         </Link>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarNavAltMarkup'
+          aria-controls='navbarNavAltMarkup'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
         >
-          <span className="navbar-toggler-icon" />
+          <span className='navbar-toggler-icon' />
         </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ml-auto align-items-center">
-            <Link to="/" className="nav-item nav-link li-color">
-              Home
-              {' '}
-              <span className="sr-only" />
+        <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+          <div className='navbar-nav ml-auto align-items-center'>
+            <Link to='/' className='nav-item nav-link li-color'>
+              Home <span className='sr-only' />
             </Link>
             {isAuthenticated() && (
-              <Link to="/house-list" className="nav-item nav-link li-color">
+              <Link to='/house-list' className='nav-item nav-link li-color'>
                 Houses
               </Link>
             )}
             {isAuthenticated() && (
-              <Link
-                to="/favourite-list"
-                className="nav-item nav-link li-color"
-              >
+              <Link to='/favourite-list' className='nav-item nav-link li-color'>
                 Favourites
               </Link>
             )}
             <button
-              type="button"
-              className="nav-item nav-link text-white btn-success"
+              type='button'
+              className='nav-item nav-link text-white btn-success'
               onClick={isAuthenticated() ? logout : login}
             >
               {isAuthenticated() ? 'Log Out' : 'Log In'}
@@ -55,11 +51,12 @@ const Navbar = ({ auth }) => {
         </div>
       </nav>
     </header>
-  );
-};
+  )
+}
 
 Navbar.propTypes = {
   auth: PropTypes.func.isRequired,
-};
-const mapStateToProps = ({ User }) => ({ User });
-export default connect(mapStateToProps)(Navbar);
+  Loading: PropTypes.bool.isRequired,
+}
+const mapStateToProps = ({ User, Loading }) => ({ User, Loading })
+export default connect(mapStateToProps)(Navbar)
